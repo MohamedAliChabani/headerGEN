01e814442fa3882a281481bd72ac66b1d3e8d0a9
dalichabani <dalichabani@dalilaptop.home.org>
2024-01-07 23:54:09 +0100

    added cli arguments parsing

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import yaml
 import gen
+import sys
 
 def write_config(config):
-    with open("test.h", "w", encoding="utf-8") as header:
+    with open(sys.argv[2], "w", encoding="utf-8") as header:
         gen.write_headers(header, config)
         gen.write_classes(header, config)
 
 def main():
-    with open("config.yml", "r", encoding="utf-8") as f:
+    if len(sys.argv) < 3:
+        print(f"Usage: python3 {sys.argv[0]} <config.yml> <header.h>")
+        return
+
+    with open(sys.argv[1], "r", encoding="utf-8") as f:
         config = yaml.safe_load(f)
         write_config(config)
 
